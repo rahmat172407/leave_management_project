05138c5735fde0db958104fcb1ace0a9564978d7
Rahmat <rahmat172407@gmail.com>
2024-09-11 13:21:00 +0600

    initialize

---
+++ front/partials/footer.php
@@ -0,0 +1,20 @@
+<div class="clearfix"></div>
+         <footer class="site-footer">
+            <div class="footer-inner bg-white">
+               <div class="row">
+                  <div class="col-sm-6">
+                     Copyright &copy; BAF Comm(U)
+                  </div>
+                  <div class="col-sm-6 text-right">
+                     Designed by <a href="">LAC Rahmat Ullah</a>
+                  </div>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <script src="./public/assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+      <script src="./public/assets/js/popper.min.js" type="text/javascript"></script>
+      <script src="./public/assets/js/plugins.js" type="text/javascript"></script>
+      <script src="./public/assets/js/main.js" type="text/javascript"></script>
+   </body>
+</html>
