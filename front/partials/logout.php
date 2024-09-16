9122ea6261cbfb895d0757bbadbc684406f4f390
Rahmat <rahmat172407@gmail.com>
2024-09-16 17:26:15 +0600

    prev

--- front/partials/logout.php
+++ front/partials/logout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-unset($_SESSION['role']);
-unset($_SESSION['id']);
-unset($_SESSION['name']);
+unset($_SESSION['ROLE']);
+unset($_SESSION['USER_ID']);
+unset($_SESSION['USER_NAME']);
 header('location:login.php');
 die();
 ?>
